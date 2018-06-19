07ffe0d6154d36aff75584952221c95ea8e7845f
Pablo Leon Rodrigues <pablo.leonrodrigues@gmail.com>
2018-06-18 23:25:46 -0300

    Adicionando select de paises

--- js/EconomicInfo.js
+++ js/EconomicInfo.js
@@ -5,25 +5,31 @@
  * @since 07/06/2018
  */
 var EconomicInfo = (function($){
-    //https://markets.ft.com/research/webservices/economicdata/v1/events?startDate=2013-09-07&endDate=2014-09-07&numResults=10000&source=cadedd8a538bb6b4
     var init = function () {
-        let url = 'https://markets.ft.com/research/webservices/economicdata/v1/events?startDate=2013-09-07&endDate=2014-09-07&numResults=400&source=cadedd8a538bb6b4';
-        console.log("Teste");
-        fetch(url) // Call the fetch function passing the url of the API as a parameter
-            .then(function(response) {
-                console.log(response.json());
-                var data = [{
-                    values: [19, 26, 55],
-                    labels: ['Residential', 'Non-Residential', 'Utility'],
-                    type: 'pie'
-                }];
-                Plotly.newPlot('graficEconomic', data);
-            })
-            .catch(function() {
-                concole.log("Error");
+        let source = '6c0f906945618c79';
+        let country;
+        Main.consulta({
+            url: 'https://markets.ft.com/research/webservices/economicdata/v1/events?startDate=2018-06-01&endDate=2018-06-18&countryCodes=BR&numResults=500&source=' + source,
+            success: function ( response ) {
+                processaResposta(response);
             }
-        );
+        })
     };
+    /**
+     * Processa retorno do web service
+     * @param response
+     */
+    var processaResposta = function ( response ) {
+        console.log("response", response);
+
+        var data = [{
+            values: [19, 26, 55],
+            labels: ['Residential', 'Non-Residential', 'Utility'],
+            type: 'pie'
+        }];
+        Plotly.newPlot('graficEconomic', data);
+    };
+
 
     /**
      * Métodos públicos
